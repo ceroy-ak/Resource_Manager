3929c4686c8554bef426558a0b794ef9fe313a7b
Ceroy <abhishek.k@technovert.com>
2021-02-09 18:38:47 +0530

    1. All Component Outline complete 2. Display all client contacts in lexographically sorted order 3. Display selected contact 4. ContextAPI completed 5. Routing completed

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import Title from "./Title";
 import LeftColumn from "./LeftColumn"
 import RightColumn from './RightColumn'
-import ClientProvider from './Contexts/ClientProvider'
+import {ClientProvider} from './Contexts/ClientProvider'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import ClientDisplay from './ClientDisplay'
+import AddClient from './AddClient'
+import UpdateClient from './UpdateClient'
 
 function App() {
   return (
@@ -10,7 +14,13 @@ function App() {
       <ClientProvider>
         <Title />
         <LeftColumn />
-        <RightColumn />
+
+        <BrowserRouter>
+        <Route path="/clients" component={RightColumn} />
+        <Route path="/clients/add" exact component={AddClient} />
+        <Route path="/clients/:id" component={ClientDisplay} />
+        </BrowserRouter>
+        
       </ClientProvider>
 
     </div>
