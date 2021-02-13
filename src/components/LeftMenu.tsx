16c3d6f6771b541c10ed078af14dca0fbeebd616
Ceroy <abhishek.k@technovert.com>
2021-02-13 13:37:33 +0530

    LeftColumn changed to LeftMenu

---
+++ src/components/LeftMenu.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function LeftMenu() {
+    return (
+        <div className="left-column ms-hiddenLgDown">
+            <>
+            </>
+        </div>
+    )
+}
+
+export default LeftMenu
